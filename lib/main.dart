f83733fde8f28b6e866349919f137126cf38c212
vleiswal <vleiswal@gmail.com>
2021-01-11 11:55:49 +0200

    start_init

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  // this is the start
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(App());
 }
-
-
-
